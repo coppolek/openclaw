0cd0037ba320305b6bd86cabcd60e836aba4d1e7
Clayton <claytonlin1110@gmail.com>
2026-04-20 10:46:09 -0500

    fix: update

--- src/config/schema.base.generated.ts
+++ src/config/schema.base.generated.ts
@@ -2792,6 +2792,13 @@ export const GENERATED_BASE_CONFIG_SCHEMA: BaseConfigSchemaResponse = {
                           type: "string",
                         },
                       },
+                      extraParams: {
+                        type: "object",
+                        propertyNames: {
+                          type: "string",
+                        },
+                        additionalProperties: {},
+                      },
                       compat: {
                         type: "object",
                         properties: {
